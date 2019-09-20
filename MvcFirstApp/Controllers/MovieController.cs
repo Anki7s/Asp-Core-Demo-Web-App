95e0d0e032cea27d70fa94fbdb9eda11970e30bd
AnkitKumar  Singh <52848024+Anki7s@users.noreply.github.com>
2019-09-20 19:02:07 +0530

    demo application with Mock model data

---
+++ MvcFirstApp/Controllers/MovieController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcFirstApp.Models;
+using MvcFirstApp.Repository;
+using System.Collections;
+
+
+namespace MvcFirstApp.Controllers
+{
+    [Route("Movie")]
+    public class MovieController : Controller
+    {
+        private IMovieRepository _movieRepository;
+
+        public MovieController(IMovieRepository movieRepository)
+        {
+            _movieRepository = movieRepository;
+
+        }
+
+        [Route("/")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            IEnumerable model = _movieRepository.GetMovieList();
+            return View(model);
+        }
+
+        [Route("Save/{id?}")]
+        [HttpGet]
+        public IActionResult Save(int id)
+        {
+            return View(_movieRepository.GetMovie(id));
+        }
+
+        [Route("Save/{id?}")]
+        [HttpPost]
+        public IActionResult Save(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("index", _movieRepository.SaveMovie(movie));
+            }
+            return View(movie);
+        }
+
+        [Route("Details/{id?}")]
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            Movie model = _movieRepository.GetMovie(id);
+            return View(model);
+        }
+
+        [Route("Delete/{id?}")]
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            Movie model = _movieRepository.GetMovie(id ?? 1);
+            return View(model);
+        }
+
+        [Route("Delete/{id?}")]
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Movie model = _movieRepository.Delete(id);
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ MvcFirstApp/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcFirstApp.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter movie name")]
+        [StringLength(20, MinimumLength = 4, ErrorMessage = "Title must be between 4 and 20 char")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please enter movie star name")]
+        [DataType(DataType.Text)]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Movie star must be between 2 and 20 char")]
+        public string MovieStar { get; set; }
+
+        [Required(ErrorMessage = "Please enter movie type")]
+        [DataType(DataType.Text)]
+        [StringLength(20, MinimumLength = 2, ErrorMessage = "Movie type must be between 2 and 20 char")]
+        public string MovieType { get; set; }
+    }
+}
